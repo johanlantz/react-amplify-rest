f5ef4d91d388c0ca91024f4cf06fb4f072b23567
Johan Lantz <johan.lantz@telefonica.com>
2020-10-29 12:22:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -8,16 +8,36 @@ import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 
-const onBtnClick = () => {
-  fetch("https://tzf1sbnlo9.execute-api.eu-west-1.amazonaws.com/dev/todo")
-  .then(response => response.json())
-  .then(data => console.log(data))
+const onBtnClick = async () => {
+  const user = await Amplify.Auth.currentAuthenticatedUser();
+  const token = user.signInUserSession.idToken.jwtToken;
+  fetch("https://tzf1sbnlo9.execute-api.eu-west-1.amazonaws.com/dev/todoist", {
+    method: 'POST', headers: {
+      Authorization: token
+    }
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+};
+
+const onGetClick = async () => {
+  const user = await Amplify.Auth.currentAuthenticatedUser();
+  const token = user.signInUserSession.idToken.jwtToken;
+  fetch("https://tzf1sbnlo9.execute-api.eu-west-1.amazonaws.com/dev/todoist", {
+    method: 'GET',
+    headers: {
+      Authorization: token
+    }
+  })
+    .then(response => console.log(response))
 };
 
 const App = () => (
   <div>
     <AmplifySignOut />
-    <input type="button" onClick={onBtnClick}></input>
+    <input type="button" onClick={onBtnClick} value="post"></input>
+
+    <input type="button" onClick={onGetClick} value="get"></input>
   </div>
 );
 
